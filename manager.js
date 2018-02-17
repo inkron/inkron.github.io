367f8900768ecaf852ae157d86a176842c00ac7c
Enclo <jelch2002@gmail.com>
2018-02-16 18:20:52 -0600

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,4 +1,4 @@
-var socket = io.connect("https://41bdc560.ngrok.io")
+var socket = io.connect("https://628832ec.ngrok.io")
 indexPage = false
 fAdded = false
 username = null
@@ -10,18 +10,25 @@ currFriends = null
 currUserChat = null
 currUserChatHistory = null
 privateIsTyping = false
+createGroup = []
+chatType = 1
+currUserData = null
+glChatId = null
+glOnUser = null
+usersTyping = []
 function login(sessionKey){
 				socket.emit("login", sessionKey)
 			}
 function sendTyping(){
 	privateIsTyping = false;
 	console.log("typingEventFalse")
-	socket.emit("typingEvent", privateIsTyping, username, currUserChat[0])
+	socket.emit("typingEvent", privateIsTyping, username, glChatId)
 }
 function render(page){
 	if (page === "homeA1"){
 		page = "home"
 		socket.emit("changePage", page, username)
+		$(".gc").remove()
 		$(".friContainer").remove()
 		$(".chatContainer").remove()
 		$(".defaultText").remove()
@@ -43,6 +50,7 @@ function render(page){
 	}else if(page === "homeA2"){
 		page = "home"
 		socket.emit("changePage", page, username)
+		$(".gc").remove()
 		$(".friContainer").remove()
 		$(".chatContainer").remove()
 		$(".defaultText").remove()
@@ -60,6 +68,7 @@ function render(page){
 	}else if(page === "sq"){
 		page = "sq"
 		socket.emit("changePage", page, username)
+		$(".gc").remove()
 		$(".friContainer").remove()
 		$(".chatContainer").remove()
 		$(".defaultText").remove()
@@ -73,6 +82,7 @@ function render(page){
 	}else if(page == ("friendsA1")){
 		page = "friends"
 		socket.emit("changePage", page, username)
+		$(".gc").remove()
 		$(".userChatTopNav").remove()
 		$(".chatContainer").remove()
 		$(".userChatLeftHalf").remove()
@@ -92,6 +102,7 @@ function render(page){
 	}else if(page == ("mailA1")){
 		page = "mail"
 		socket.emit("changePage", page, username)
+		$(".gc").remove()
 		$(".friContainer").remove()
 		$(".chatContainer").remove()
 		$(".defaultText").remove()
@@ -103,6 +114,7 @@ function render(page){
 		$(".bim").attr("src", "app-assets/bellS.svg")
 		$(".fim").attr("src", "app-assets/userN.svg")
 	}else if(page == "uProfileA2"){
+		$(".gc").remove()
 		page = "uProfile"
 		socket.emit("changePage", page, username)
 		$(".defaultText").remove()
@@ -123,10 +135,18 @@ function render(page){
 		socket.emit("changePage", page, username)
 		$(".bottomNav").remove()
 		$(".friContainer").remove()
+		$(".gc").remove()
 		$(".topNav").remove()
 		$("center").append("<div class='userChatTopNav'><div class='bbc'><img src='app-assets/backarrow.svg' width='30px' height='30px'></img></div><div class='userChatUName'>" + currUserChat[0] + "</div><div class='userChatName'>" + currUserChat[1] + "</div></div>")
 		$("body").append("<div class='chatContainer'></div>")
 		$("center").append("<div class='userChatBottomNav'><form class='usform'><input class='userChatInput'></input></div></div>")
+	}else if(page == "groupCreate"){
+		createGroup = [username]
+		$(".gc").remove()
+		$(".bottomNav").remove()
+		$(".friContainer").remove()
+		$(".topNav").remove()
+		$("center").append("<div class='gc'><div class='gcTopNav'><div class='bbc'><img src='app-assets/backarrow.svg' width='30px' height='30px'></img></div><div class='gcTitle'>Create Group</div></div><div class='gcInputDiv'><div class='gcTitle2'>Choose a name</div><input class='gcGroupName' placeholder='Desired Group Name'></input></div></button><div class='gcTitle3'>Add Friends</div><div class='gcContainer'></div><button class='gcCreateButton'>Create</div>")
 	}
 }
 $(function(){
@@ -165,28 +185,72 @@ $(function(){
 
 	socket.on("connectToUserResponse", function(chatID, array){
 		console.log(array)
+		if (array[1].length == 0){
+			chatType = 2;
+		}else{
+			chatType = 1;
+		}
 		currUserChat = array
+		glChatId = chatID
 		render("userChat")
 		socket.emit("getChatHistory", chatID)
 	})
 
 	socket.on("incomingMessage", function(payload){
-		$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'>" + payload[1] + "</div></div></div>")
+		if (payload[0] != username){
+				if (chatType == 1){
+						$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'>" + payload[1] + "</div></div></div>")
+					}else{
+						if (payload[0] != glOnUser){
+							$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'><div id='chatBoxAuthor' class='gcb" + payload[0] + "'>" + payload[0] + "</div><br><div class='chatBoxOUContent'>" + payload[1] + "</div></div></div></div>")
+							if (currUserData[0].includes(payload[0])){
+								var rgbArr = currUserData[2][currUserData[0].indexOf(payload[0])]
+								$('.gcb' + payload[0]).css("color", "rgb(" + rgbArr[0] + ", " + rgbArr[1] + " , " + rgbArr[2] + ")")
+							}
+							glOnUser - payload[0]
+						}else{
+							$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'><div class='chatBoxOUContent2'>" + payload[1] + "</div></div></div></div>")
+
+						}
+					}
+		}else{
+			$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxUser'>" + payload[1] + "</div></div></div>")
+		}
 		$('body').animate({scrollTop: $('body').get(0).scrollHeight}, 2000);
 	});
 
 	socket.on("getChatHistoryResponse", function(history){
 		currUserChatHistory = history
+		var onUser = null
+		glOnUser = null
 		if(history.length != 0){
 			for (var i in history){
 				if (history[i][0] === username){
+					onUser = null
 					$(".chatContainer").append("<div class='cbum'><div class='cbur'><div class='chatBoxUser' dir='ltr'>" + history[i][1] + "</div></div></div>")
 				}else{
-					$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'>" + history[i][1] + "</div></div></div>")
+					if (chatType == 1){
+						$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'>" + history[i][1] + "</div></div></div>")
+					}else{
+						if (onUser != history[i][0]){
+							$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'><div id='chatBoxAuthor' class='gcb" + history[i][0] + "'>" + history[i][0] + "</div><br><div class='chatBoxOUContent'>" + history[i][1] + "</div></div></div></div>")
+							if (currUserData[0].includes(history[i][0])){
+								var rgbArr = currUserData[2][currUserData[0].indexOf(history[i][0])]
+								$('.gcb' + history[i][0]).css("color", "rgb(" + rgbArr[0] + ", " + rgbArr[1] + " , " + rgbArr[2] + ")")
+							}
+							onUser = history[i][0]
+						}else{
+							$(".chatContainer").append("<div class='cboum'><div class='cbour'><div class='chatBoxOtherUser'><div class='chatBoxOUContent2'>" + history[i][1] + "</div></div></div></div>")
+						}
+					}
 				}
 			}
 			$('body').animate({scrollTop: $('body').get(0).scrollHeight}, 2000);
 		}
+	})
+
+	socket.on("updateUserData", function(data){
+		currUserData = data;
 	})
 
 	socket.on("getNotificationsResponse", function(notifications){
@@ -210,6 +274,10 @@ $(function(){
 		}
 	})
 
+	socket.on("createGroupResponse", function(name){
+		socket.emit("connectToUser", false, name, username)
+	})
+
 	socket.on("getUserProfileResponse", function(payload, posts, fad){
 			fAdded = fad
 			console.log(JSON.stringify(payload))
@@ -222,31 +290,70 @@ $(function(){
 			}
 	})
 
+	socket.on('getFriendsFNGResponse', function(data){
+		render('groupCreate')
+		if (data.length != 0){
+			for (var i in data[0]){
+				$(".gcContainer").append("<div class='gcFriendBox'><div class='gcBoxUName'>" + data[0][i] + "</div><div class='gcBoxName'>" + data[1][i] + "</div><div class='gcSelector'></div></div>")
+			}
+		}
+
+	})
+
 	socket.on("typingEventResponse", function(event, user){
 		if (event){
-			$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>" + user + " is typing...</div></div>")
-		}else{
 			$(".typingEvent").remove()
+			if (!(usersTyping.includes(user))){
+				usersTyping.push(user)
+			}
+			if (usersTyping.length == 1){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>" + usersTyping[0] + " is typing...</div></div>")
+			}else if (usersTyping.length == 2){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>" + usersTyping[0] + " and " + usersTyping[1] +" are typing...</div></div>")
+			}else if (usersTyping.length == 3){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>" + usersTyping[0] + ", " + usersTyping[1] + "and " + usersTyping[2] +" are typing...</div></div>")
+			}else if (usersTyping.length > 3){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>Multiple people are typing...</div></div>")
+			}
+		}else{
+			usersTyping.splice(usersTyping.indexOf(user), 1)
+			$(".typingEvent").remove()
+			if (usersTyping.length == 1){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>" + usersTyping[0] + " is typing...</div></div>")
+			}else if (usersTyping.length == 2){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>" + usersTyping[0] + " and " + usersTyping[1] +" are typing...</div></div>")
+			}else if (usersTyping.length == 3){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>" + usersTyping[0] + ", " + usersTyping[1] + "and " + usersTyping[2] +" are typing...</div></div>")
+			}else if (usersTyping.length > 3){
+				$(".userChatTopNav").append("<div class='typingEvent'><div class='chatBoxUserTypingText'>Multiple people are typing...</div></div>")
+			}
 		}
 	})
 
-	socket.on("getFriendsResponse", function(friends){
+	socket.on("getChatsResponse", function(friends, das){
 		currFriends = friends
+		var dasCounter = 0
 		render("friendsA1")
 		if (friends[0].length != 0){
 			$(".defaultText").remove()
 			for (var i in friends[0]){
-					if ((parseInt(i) +  1) != friends[0].length){
-						console.log(i + 1)
-						console.log(friends[0].length)
+					console.log(i + 1)
+					console.log(friends[0].length)
+					if (friends[1][i] != 0){
 						$(".friContainer").append("<div class='friBox'><div class='friBoxUName'>" + friends[0][i] + "</div><div class='friBoxName'>" + friends[1][i] +"</div><div class='friButtons'><button class='friButton1' id='" + friends[0][i] + "'>Send Message</button><button class='friButton2' id='" + friends[0][i] + "'>Remove</button></div></div>")
 					}else{
-						$(".friContainer").append("<div class='friBoxL'><div class='friBoxUName'>" + friends[0][i] + "</div><div class='friBoxName'>" + friends[1][i] +"</div><div class='friButtons'><button class='friButton1' id='" + friends[0][i] + "'>Send Message</button><button class='friButton2' id='" + friends[0][i] + "'>Remove</button></div></div>")
+						$(".friContainer").append("<div class='friBox'><img class='gIcon' src='app-assets/groupMessagesIcon.svg'></img><div class='friBoxUName'>" + friends[0][i] + "</div><div class='friBoxName'>" + friends[1][i] +"</div><div class='friButtons'><button class='friButton1g' id='" + friends[0][i] + "'>Send Message</button><button class='friButton2g' id='" + das[dasCounter] + "'>Leave</button></div></div>")
+						dasCounter += 1;
 					}
 			}
+			$(".friContainer").append("<div class='friBoxButtons'><button class='friCreateGroupButton'>Create Group</button></div>")
 		}else{
 			$(".friContainer").append("<div class='defaultText' id='a1'>Friend List Empty</div>")
 		}
+	})
+
+	socket.on("getChatsReload", function(){
+		socket.emit("getChats", username)
 	})
 
 	$('body').on('input', '.searchQuery', function(){
@@ -268,11 +375,11 @@ $(function(){
 	});
 	$('body').on('click', '.bbc', function(){
 		socket.emit("disconnectFromChat", username)
-		socket.emit("getFriends", username)
+		socket.emit("getChats", username)
 	});
 	$('body').on('click', '#friends', function(){
 		if (!(page === "friends")){
-			socket.emit("getFriends", username)
+			socket.emit("getChats", username)
 		}
 	});
 	$('body').on('click', '#home', function(){
@@ -316,8 +423,27 @@ $(function(){
 	})
 	$('body').on('click', ".friButton1", function(){
 		var user = $(this).attr("id")
-		socket.emit("connectToUser", user, username)
+		socket.emit("connectToUser", true, user, username)
 	});
+
+	$('body').on('click', ".friButton1g", function(){
+		var user = $(this).attr("id")
+		socket.emit("connectToUser", false, user, username)
+	});
+
+	$('body').on('click', ".friButton2g", function(){
+		var chid = $(this).attr("id")
+		console.log(chid)
+		socket.emit("removeFromGroup", username, chid)
+		$(this).parent().parent().remove()
+		currFriends[0].splice(currFriends[2].indexOf(chid), 1)
+		currFriends[1].splice(currFriends[2].indexOf(chid), 1)
+		if (currFriends[0].length == 0){
+			$("center").append("<div class='defaultText' id='a1'>Friend List Empty</div>")
+			$(".friBoxButtons").remove()
+		}
+	});
+
 	$('body').on('click', ".friButton2", function(){
 		var user = $(this).attr("id")
 		$(this).parent().parent().remove()
@@ -326,14 +452,30 @@ $(function(){
 		socket.emit("removeFriend", user, username)
 		if (currFriends[0].length == 0){
 			$("center").append("<div class='defaultText' id='a1'>Friend List Empty</div>")
-		}else{
-			$(".friContainer").children().last().attr("class", "friBoxL")
+			$(".friBoxButtons").remove()
 		}
 	});
+
+	$("body").on('click', '.friCreateGroupButton', function(){
+		socket.emit("getFriendsFNG", username)
+	})
+
+	$("body").on('click', '.gcSelector', function(){
+		$(this).attr("class", 'gcSelector2')
+		createGroup.push($(this).prev().prev().text())
+		console.log(createGroup)
+	});
+
+	$("body").on('click', '.gcSelector2', function(){
+		createGroup.splice(createGroup.indexOf($(this).prev().prev().text()), 1)
+		$(this).attr("class", 'gcSelector')
+		console.log(createGroup)
+	});
+
 	$('body').on('submit', '.usform', function(e){
 		e.preventDefault();
 		$(".chatContainer").append("<div class='cbum'><div class='cbur'><div class='chatBoxUser' dir='ltr'>" +  $(".userChatInput").val() + "</div></div></div>")
-		socket.emit("sendMessage", $(".userChatInput").val(), username, currUserChat[0])
+		socket.emit("sendMessage", chatType, $(".userChatInput").val(), username, currUserChat[0])
 		sendTyping()
 		$('body').animate({scrollTop: $('body').get(0).scrollHeight}, 2000);
 		$(".userChatInput").val("")
@@ -345,9 +487,12 @@ $(function(){
 		if (!(privateIsTyping)){
 			privateIsTyping = true
 			console.log("typingEventTrue")
-			socket.emit("typingEvent", privateIsTyping, username, currUserChat[0])
+			socket.emit("typingEvent", privateIsTyping, username, glChatId)
 		}
 			clearTimeout(privateTimer)
 			privateTimer = setTimeout(sendTyping, 1000)
+	})
+	$('body').on('click', '.gcCreateButton', function(){
+		socket.emit("createGroup", createGroup, $(".gcGroupName").val())
 	})
 })
